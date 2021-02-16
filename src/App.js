bcee0bc1b4dafcaf58516ad24e4307a820d908e4
eladcandroid <eladc.android@gmail.com>
2021-02-16 10:06:44 +0200

    Finish refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import Todo from './components/Todo/Todo'
-import AddTodo from './components/AddTodo/AddTodo'
+import React, { useEffect, useState } from "react";
+import Todo from "./components/Todo/Todo";
+import AddTodo from "./components/AddTodo/AddTodo";
 // import './App.css';
 
 // import shuki, {arr1} from './efrat.js'
-const App = () =>  {
+const App = () => {
   const [todos, setTodos] = useState([]);
-  
-  useEffect(() => {
-    console.log('todos', todos);
-  }, [todos]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
@@ -17,14 +13,24 @@ const App = () =>  {
       .then((data) => setTodos(data));
   }, []);
 
-    return (
-      <div>
-        <AddTodo onAdd={this.addTodo} />
-        {todos.map((todo) => (
-          <Todo key={todo.id} id={todo.id} title={todo.title} />
-        ))}
-      </div>
-    );
-}
+  const addTodo = (title) => {
+    const newTodo = {
+      id: todos.length + 1,
+      title,
+      userId: 0,
+      completed: false,
+    };
+    setTodos([newTodo, ...todos]);
+  };
+
+  return (
+    <div>
+      <AddTodo onAdd={addTodo} />
+      {todos.map((todo) => (
+        <Todo key={todo.id} id={todo.id} title={todo.title} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
